d7a7263644bb5e4b8f7e09d495e6e12496a2e3d3
ashish <aashish7322@gmail.com>
2022-10-17 19:35:06 +0530

    Added: TicTacToe Game Class

---
+++ src/main/java/com/bridgelabz/EmployeePay/TicTacToeGame.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.EmployeePay;
+
+public class TicTacToeGame {
+	public void createBoard() {
+		char[] board = new char[10];
+		board[1] = ' ';
+		board[2] = ' ';
+		board[2] = ' ';
+		board[3] = ' ';
+		board[4] = ' ';
+		board[5] = ' ';
+		board[6] = ' ';
+		board[7] = ' ';
+		board[8] = ' ';
+		board[9] = ' ';
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
