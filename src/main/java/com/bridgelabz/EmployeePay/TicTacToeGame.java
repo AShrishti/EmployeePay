0b684fdb6c2ccc131c21604028a6d70235ae69d4
ashish <aashish7322@gmail.com>
2022-10-17 20:50:41 +0530

    Added : Choose Player for Board Game

--- src/main/java/com/bridgelabz/EmployeePay/TicTacToeGame.java
+++ src/main/java/com/bridgelabz/EmployeePay/TicTacToeGame.java
@@ -1,18 +1,40 @@
 package com.bridgelabz.EmployeePay;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TicTacToeGame {
 	/**
 	 * @describe Created Board
 	 */
 	public void createBoard() {
 		char[] board = new char[10];
-		for (char i = 1; i < 10; i++)
+		for (int i = 1; i < board.length; i++)
 			board[i] = ' ';
 
+		for (char b : board)
+			System.out.print(b);
+
+	}
+
+	public void choosePlayer() {
+		char computer;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Character X or O");
+
+		char player = sc.nextLine().charAt(0);
+		if (player == 'X') {
+			computer = 'O';
+		} else
+			computer = 'X';
+		sc.close();
 	}
 
 	public static void main(String[] args) {
 		TicTacToeGame boardObj = new TicTacToeGame();
 		boardObj.createBoard();
+		boardObj.choosePlayer();
 	}
 }
